e9751fc3e9ec4dbdfa99f618cd9214bc7fc7e232
AminLexs <fedchenko.leha@gmail.com>
2021-10-23 16:23:31 +0300

    added https server, modified gitignore

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require('express');
+const https = require('https');
+const http = require('http');
 const bodyParser = require('body-parser')
 const initDB = require('./common/initializationDB')
 const mongoUtils = require('./common/mongoUtils')
 const electionsRouter = require('./routers/electionsRouter')
 const homeRouter = require('./routers/homeRouter')
+const fs = require("fs");
 
-const PORT = process.env.PORT
+const key = fs.readFileSync('./sslcert/key.key');
+const cert = fs.readFileSync('./sslcert/certificate.crt');
+
 const app = express() // initialization server
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,10 +21,17 @@ app.use('/', homeRouter)
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
+
+const httpServer = http.createServer(app);
+const httpsServer  = https.createServer({key: key, cert: cert }, app);
+const httpsPort = process.env.HTTPSPORT
+const httpPort = process.env.HTTPPORT
 mongoUtils.connectToDB( (err) => {
     if (err) console.log(err);
     initDB.initDefaultDB()
-    app.listen(PORT, () => console.log(`Running on port ${PORT}.`))
+    //app.listen(PORT, () => console.log(`Running on port ${PORT}.`))
+    httpServer.listen(httpPort,() => { console.log(`Http server is listening on ${httpPort}`) });
+    httpsServer.listen(httpsPort, () => { console.log(`Https server is listening on ${httpsPort}`) });
 })
 
 
