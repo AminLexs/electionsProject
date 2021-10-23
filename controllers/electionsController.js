7e429fc326f21c393c7a3f4aa774c72e5a107606
AminLexs <fedchenko.leha@gmail.com>
2021-10-23 14:31:35 +0300

    deleted old configs, added sending vote

--- controllers/electionsController.js
+++ controllers/electionsController.js
@@ -2,12 +2,28 @@ const getClientIp = require('request-ip').getClientIp;
 const getGeo = require('geoip-country').lookup;
 const Candidate = require("../models/elections/candidate");
 const Vote = require("../models/elections/vote");
+const {getDataModel,ObjectId} = require("../common/mongoUtils");
+
+function requestCountry(req, privateIpCountry) {
+    let privateIpReg = /^(?:192\.168\.[0-9]+\.[0-9]+|10\.[0-9]+\.[0-9]+\.[0-9]+|172\.(1[6-9]|2[0-9]|3[0-2])\.[0-9]+\.[0-9]+|127\.0\.0\.1)$/;
+    let ip = typeof req === 'string' ? req : getClientIp(req), geo = getGeo(ip);
+    return geo ? geo.country : (privateIpCountry ? privateIpReg.test(ip) && privateIpCountry : false);
+}
 
 exports.getCandidates = async function (request, response) {
 
     response.send('candidates')
 };
 exports.sendVote = function (request, response) {
-    response.send('vote sent')
-
+    //  if(requestCountry(request)===config.get('permittedCountryCode')){
+    let vote = new Vote({_id:  new ObjectId(),
+        passportNumber: request.body.passportNumber,
+        candidateId:request.body.candidateId});
+    vote.save(function (err) {
+        if (err) return response.send(err);
+        response.send({});
+    })
+    //}else{
+    //  response.status(403).send({message:'Your country isn\'t permitted'});
+    //}
 };
